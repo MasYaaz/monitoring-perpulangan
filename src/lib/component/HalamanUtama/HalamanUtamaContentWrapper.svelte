a961c6081a86fbec3c6c8fb1da64b79d29a75155
Aflah Mahdi Yazdi <Diyaz.hal22@gmail.com>
2025-12-12 11:00:27 +0700

    Revisi tinggi section

--- src/lib/component/HalamanUtama/HalamanUtamaContentWrapper.svelte
+++ src/lib/component/HalamanUtama/HalamanUtamaContentWrapper.svelte
@@ -4,9 +4,11 @@
 	import PictureContent2 from './PictureContent2.svelte';
 </script>
 
-<div class="z-10 flex h-full w-full flex-col items-center pt-20 md:pt-25 lg:pt-25 xl:pt-30">
+<div
+	class="z-10 flex h-screen w-full flex-col items-center pt-25 md:pt-25 lg:h-full lg:pt-25 xl:pt-30"
+>
 	<div
-		class="flex h-full w-full max-w-md flex-col items-center justify-center gap-5 px-5 md:max-w-3xl md:px-10 lg:h-full lg:max-w-4xl lg:gap-4 xl:grid xl:max-w-7xl xl:grid-cols-6 xl:gap-6"
+		class="flex h-full w-full max-w-md flex-col items-center justify-center px-5 md:max-w-3xl md:gap-5 md:px-10 lg:max-w-4xl lg:gap-4 xl:grid xl:max-w-7xl xl:grid-cols-6 xl:gap-6"
 	>
 		<!-- Kolom 1: Gambar tunggal -->
 		<PictureContent1 />
